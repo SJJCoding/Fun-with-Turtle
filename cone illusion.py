fe357c69cdd2d40eedddea0231ed39a249125f21
SJJCoding <SJJCODING@gmail.com>
2016-05-06 17:37:52 -0700

    Create cone illusion.py

---
+++ cone illusion.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+wn = turtle.Screen()
+name = "Ian"
+wn.bgcolor("red")
+snappy = turtle.Turtle()
+snappy.speed(99)
+x = 0
+
+
+snappy.pendown() 
+snappy.circle(50)
+#while x < 360: 
+snappy.pendown() 
+snappy.circle(60)    
+#    snappy.forward(1)
+#    snappy.right(1)
+snappy.pendown() 
+snappy.circle(70)
+snappy.pendown() 
+snappy.circle(80)    
+snappy.pendown() 
+snappy.circle(90)
+snappy.pendown() 
+snappy.circle(100)
+#x = x+1 # adds 1 to the value of x,
+wn.exitonclick()
